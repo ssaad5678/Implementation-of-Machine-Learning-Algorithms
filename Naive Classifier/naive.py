ce12218ef3e329caec2eb9f2d82f81e6ccb03298
ssaad5678 <84627426+ssaad5678@users.noreply.github.com>
2023-05-16 22:11:59 +0530

    Update naive.py

--- Naive Classifier/naive.py
+++ Naive Classifier/naive.py
@@ -1,42 +1,49 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-# Read data from CSV file
-msg = pd.read_csv('naivetext1.csv')
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+
+# Read the dataset
+msg = pd.read_csv('naivetext1.csv', names=['message', 'label'])
 print('The dimensions of the dataset:', msg.shape)
 
-# Create labelnum column from label
-msg['labelnum'] = msg['label'].map({'pos': 1, 'neg': 0})
-X=msg.message
-y=msg.labelnum
-print(X)
-print(y)
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(msg['message'], msg['labelnum'], random_state=1)
-print('The shape of ytest:', y_test.shape)
-print('The shape of xtrain:', X_train.shape)
-print('The shape of xtest:', X_test.shape)
-print('The shape of ytrain:', y_train.shape)
+# Map labels to numerical values
+msg['labelnum'] = msg.label.map({'pos': 1, 'neg': 0})
+X = msg.message
+y = msg.labelnum
 
-# Create count vectorizer and transform training data
-vect = CountVectorizer()
-X_train_dtm = vect.fit_transform(X_train)
-print('Number of features extracted from messages:', len(vect.get_feature_names()))
+# Handle missing values in y
+y = y.fillna(0)  # Replace NaN values with 0 or any other appropriate value
 
-# Create DataFrame showing the frequency of each feature in the training dataset
-df = pd.DataFrame(X_train_dtm.toarray(), columns=vect.get_feature_names())
-print('Frequency of each feature in the training dataset:')
+# Split the dataset into train and test data
+xtrain, xtest, ytrain, ytest = train_test_split(X, y)
+print("ytest shape:", ytest.shape)
+print("xtrain shape:", xtrain.shape)
+print("xtest shape:", xtest.shape)
+print("ytrain shape:", ytrain.shape)
+
+# Feature Extraction to convert text to document term matrix
+count_vect = CountVectorizer()
+xtrain_dtm = count_vect.fit_transform(xtrain)
+xtest_dtm = count_vect.transform(xtest)
+print("Feature names:", count_vect.get_feature_names_out())
+
+df = pd.DataFrame(xtrain_dtm.toarray(), columns=count_vect.get_feature_names_out())
+print("Dataframe representation:")
 print(df)
+print("Sparse matrix representation:")
+print(xtrain_dtm)
 
-# Train Naive Bayes classifier
-nb = MultinomialNB()
-nb.fit(X_train_dtm, y_train)
+# Training Naive Bayes (NB) classifier on training data
+clf = MultinomialNB().fit(xtrain_dtm, ytrain)
+predicted = clf.predict(xtest_dtm)
 
-# Predict on testing data and evaluate performance
-X_test_dtm = vect.transform(X_test)
-y_pred_class = nb.predict(X_test_dtm)
-print('Accuracy:', metrics.accuracy_score(y_test, y_pred_class))
-print('Confusion matrix:')
-print(metrics.confusion_matrix(y_test, y_pred_class))
-print('Recall score:', metrics.recall_score(y_test, y_pred_class))
-print('Precision score:', metrics.precision_score(y_test, y_pred_class))
+# Printing accuracy metrics
+print('Accuracy metrics')
+print('Accuracy of the classifier is', metrics.accuracy_score(ytest, predicted))
+print('Confusion matrix')
+print(metrics.confusion_matrix(ytest, predicted))
+print('Recall and Precision')
+print(metrics.recall_score(ytest, predicted, zero_division=1))
+print(metrics.precision_score(ytest, predicted, zero_division=1))
